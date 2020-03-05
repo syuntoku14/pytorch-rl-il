1204c2d59f83d52e5ba30bb505de57d4d5486244
syuntoku14 <syuntoku14@keio.jp>
2020-03-05 21:09:56 +0900

    Update environments/__init__.py

--- rlil/environments/__init__.py
+++ rlil/environments/__init__.py
@@ -1,6 +1,6 @@
 from .abstract import Environment
 from .gym import GymEnvironment
 from .state import State
-from .action import Action
+from .action import Action, action_decorator
 
 __all__ = ["Environment", "State", "GymEnvironment", "Action"]
