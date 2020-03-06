5ce903868a3a42f064774255fbdd1ab8cab01095
syuntoku14 <syuntoku14@keio.jp>
2020-03-06 18:20:05 +0900

    Modify: target was ignored by .gitignore

---
+++ rlil/approximation/target/trivial.py
@@ -0,0 +1,16 @@
+import torch
+from .abstract import TargetNetwork
+
+class TrivialTarget(TargetNetwork):
+    def __init__(self):
+        self._model = None
+
+    def __call__(self, *inputs):
+        with torch.no_grad():
+            return self._model(*inputs)
+
+    def init(self, model):
+        self._model = model
+
+    def update(self):
+        pass
